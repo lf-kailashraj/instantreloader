9a316f4ed18ce6930ae08ed89e7f1ff95ec97e71
FriedDust <frieddust@FriedDusts-MacBook-Pro.local>
2015-07-04 01:04:12 +0545

    Initial commit

---
+++ src/main/java/org/lftechnology/outlier/instantreloader/App.java
@@ -0,0 +1,13 @@
+package org.lftechnology.outlier.instantreloader;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
