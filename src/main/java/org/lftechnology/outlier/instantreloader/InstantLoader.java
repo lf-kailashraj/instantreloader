12c6403288a175a1367cbe3642116725724b31e2
anish <anees.manandhar@gmail.com>
2015-07-04 05:30:02 +0545

    Added InstantLoader to test premain

---
+++ src/main/java/org/lftechnology/outlier/instantreloader/InstantLoader.java
@@ -0,0 +1,15 @@
+package org.lftechnology.outlier.instantreloader;
+
+import java.lang.instrument.Instrumentation;
+
+/**
+ * 
+ * @author anish
+ *
+ */
+public class InstantLoader {
+
+	public static void testPremain(String agentArgs, Instrumentation inst) {
+		//TODO
+	}
+}
