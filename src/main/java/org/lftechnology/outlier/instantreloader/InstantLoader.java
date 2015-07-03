924d4ae574afeb6f2602cf738d356b1e4d33d740
anish <anees.manandhar@gmail.com>
2015-07-04 01:11:25 +0545

    Created premain method with Instrumentation param to intercept and classes before beoing loaded by JVM

--- src/main/java/org/lftechnology/outlier/instantreloader/InstantLoader.java
+++ src/main/java/org/lftechnology/outlier/instantreloader/InstantLoader.java
@@ -1,10 +1,16 @@
 package org.lftechnology.outlier.instantreloader;
 
+import java.lang.instrument.Instrumentation;
+
 /**
  * 
  * @author anish
  *
  */
 public class InstantLoader {
+	
+	public static void premain(String agentArgs, Instrumentation inst) {
+		
+	}
 
 }
