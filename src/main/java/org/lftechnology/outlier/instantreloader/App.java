89fe768342f9c1ef29adb4642fdc1a9eff00dbe0
FriedDust <frieddust@FriedDusts-MacBook-Pro.local>
2015-07-04 01:12:16 +0545

    App modified

--- src/main/java/org/lftechnology/outlier/instantreloader/App.java
+++ src/main/java/org/lftechnology/outlier/instantreloader/App.java
@@ -4,10 +4,23 @@ package org.lftechnology.outlier.instantreloader;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public App() {
+		try {
+			for (;;) {
+				doSomething();
+				Thread.sleep(2000);
+			}
+		} catch (InterruptedException ie) {
+			ie.printStackTrace();
+		}
+	}
+
+	private void doSomething() {
+		System.out.println("from do something");
+	}
+
+	public static void main(String[] args) {
+		new App();
+	}
 }
