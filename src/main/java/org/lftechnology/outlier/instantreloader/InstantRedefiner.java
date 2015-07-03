7993117dfdc4f208e1cfe8f852b374869769261b
FriedDust <frieddust@FriedDusts-MacBook-Pro.local>
2015-07-04 01:38:29 +0545

    Instant redefiner

--- src/main/java/org/lftechnology/outlier/instantreloader/InstantRedefiner.java
+++ src/main/java/org/lftechnology/outlier/instantreloader/InstantRedefiner.java
@@ -1,6 +1,8 @@
 package org.lftechnology.outlier.instantreloader;
 
+import java.lang.instrument.ClassDefinition;
 import java.lang.instrument.Instrumentation;
+import java.lang.instrument.UnmodifiableClassException;
 
 public class InstantRedefiner {
 	private static Instrumentation instrumentation;
@@ -10,6 +12,13 @@ public class InstantRedefiner {
 	}
 
 	public static void redefine(Class<?> klass, byte[] classFile) {
-
+		try {
+			instrumentation.redefineClasses(new ClassDefinition(klass,
+					classFile));
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (UnmodifiableClassException e) {
+			e.printStackTrace();
+		}
 	}
 }
