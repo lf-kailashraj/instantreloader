4c5a338ae5fc3488f75c4b2d9f867f64238dbaa4
anish <anees.manandhar@gmail.com>
2015-07-04 02:16:09 +0545

    Created property utils to validate reload property file

---
+++ src/main/java/org/lftechnology/outlier/instantreloader/utils/PropertyUtils.java
@@ -0,0 +1,27 @@
+package org.lftechnology.outlier.instantreloader.utils;
+
+import java.util.Properties;
+
+/**
+ * 
+ * @author anish
+ *
+ */
+public class PropertyUtils {
+
+	
+	/**
+	 * 
+	 * @param props
+	 */
+	public static void checkRequiredProps(Properties props) {
+		System.out.println("Checking properties files : " + props.toString());
+		String[] requiredProps = { "reload.packages", "reload.dir","class.loader.suffix" };
+		for (String s : requiredProps) {
+			if (props.getProperty(s) == null || props.getProperty(s).equals("")) {
+				System.err.println("Agent error! Property not specified in reload.properties:"+ s);
+				System.exit(-1);
+			}
+		}
+	}
+}
